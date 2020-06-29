3e3c419f705f420d2a4bcd401b8a648dca818b0a
AsmitaChaudhari <asmita.ce55@gmail.com>
2020-06-29 18:28:39 -0300

    created get request for accessing single data

--- api/route/user.js
+++ api/route/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const router = express.Router();
 
 const User = require('../model/user');
+const Joi = require('joi');
 
 router.get('/get_users',(req,res)=>{
     User.find()
@@ -17,7 +18,23 @@ router.get('/get_users',(req,res)=>{
             error: err
         });
     });
-})
+});
+
+router.get('/get_users/:username',(req,res)=>{
+    User.findOne({username:req.params.username})
+    .exec()
+    .then(data=>{
+        console.log(data);
+        res.status(200).json(data);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
 
 
 
